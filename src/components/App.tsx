fe73fae433bb4662af202bb9932b4434cce469a1
yubessy <yubessy@gmail.com>
2020-08-31 22:36:27 +0900

    invert vertical index

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,39 +1,51 @@
 import React, { FC, useState, useEffect } from 'react'
 import './App.css'
 
-import { Point, Line, lines } from '../rule'
+import { Point, Index, indices, N_INDICES } from '../rule'
 
 const C = 50
+const boardSize = (N_INDICES + 1) * C
 
 const App: FC = () => {
   const [moves, setMoves] = useState<Point[]>([])
   useEffect(
     () => {
-      setMoves([[8, 8], [8, 7], [10, 6]]) // D3
+      setMoves([[8, 8], [8, 9], [10, 10]]) // D3
     },
     []
   )
   const onClick = (e: React.MouseEvent<SVGElement, MouseEvent>) => {
     const base = e.currentTarget.getBoundingClientRect()
-    const [rawX, rawY] = [e.clientX - base.x, e.clientY - base.y]
-    const p: Point = [adjustLine(rawX / C), adjustLine(rawY / C)]
+    const [x, y] = [e.clientX - base.x, e.clientY - base.y]
+    const p: Point = [adjust(x / C), adjust((boardSize - y) / C)]
     setMoves((prev) => [...prev, p])
   }
   return (
     <div className="App">
-      <svg width={16 * C} height={16 * C} onClick={onClick}>
-        { lines.map(
-          (x, i) => <line key={i} x1={x * C} y1={1 * C} x2={x * C} y2={15 * C} stroke="black" />
-        )
+      <svg width={boardSize} height={boardSize} onClick={onClick}>
+        {
+          // vertical lines, left to right
+          indices.map(
+            (i, k) => {
+              const x = i * C
+              return <line key={k} x1={x} y1={1 * C} x2={x} y2={N_INDICES * C} stroke="black" />
+            }
+          )
         }
-        { lines.map(
-          (y, j) => <line key={j} x1={1 * C} y1={y * C} x2={15 * C} y2={y * C} stroke="black" />
-        )
+        {
+          // horizontal lines, bottom to top
+          indices.map(
+            (j, k) => {
+              const y = (N_INDICES - j + 1) * C
+              return <line key={k} x1={1 * C} y1={y} x2={N_INDICES * C} y2={y} stroke="black" />
+            }
+          )
         }
         { moves.map(
-          ([x, y], n) => {
+          ([i, j], n) => {
             const fill = n % 2 === 0 ? 'black' : 'white'
-            return <circle key={n} cx={x * C} cy={y * C} r={C / 2 - 1} fill={fill} stroke="black" />
+            const [cx, cy] = [i * C, (N_INDICES - j + 1) * C]
+            return <circle key={n} cx={cx} cy={cy} r={C / 2 - 1} fill={fill} stroke="black" />
           }
         )
         }
@@ -42,14 +54,14 @@ const App: FC = () => {
   )
 }
 
-const adjustLine = (n: number): Line => {
+const adjust = (n: number): Index => {
   const r = Math.round(n)
   if (r < 1) {
     return 1
   } else if (r > 15) {
     return 15
   } else {
-    return r as Line
+    return r as Index
   }
 }
 
--- src/rule/foundation.ts
+++ src/rule/foundation.ts
@@ -1,5 +1,9 @@
-export const lines = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] as const
+export const N_INDICES = 15
 
-export type Line = typeof lines[number]
+export const N_WIN = 5
 
-export type Point = [Line, Line]
+export const indices = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] as const
+
+export type Index = typeof indices[number]
+
+export type Point = [Index, Index]
