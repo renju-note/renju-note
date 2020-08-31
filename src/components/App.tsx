dc8a5cb4a5348f3694fb4ab7df6570997764e6b0
yubessy <yubessy@gmail.com>
2020-08-31 20:21:40 +0900

    start implementation of rule

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,35 +1,36 @@
 import React, { FC, useState, useEffect } from 'react'
 import './App.css'
 
+import { Point, Line, lines } from '../rule'
+
 const C = 50
 
 const App: FC = () => {
-  const points = new Array(15).fill(null).map((_, i) => i + 1)
-  console.log(points)
-  const [stones, setStones] = useState<[number, number][]>([])
+  const [moves, setMoves] = useState<Point[]>([])
   useEffect(
     () => {
-      setStones([[8, 8], [8, 7], [10, 6]]) // D3
+      setMoves([[8, 8], [8, 7], [10, 6]]) // D3
     },
     []
   )
   const onClick = (e: React.MouseEvent<SVGElement, MouseEvent>) => {
     const base = e.currentTarget.getBoundingClientRect()
-    const [x, y] = [e.clientX - base.x, e.clientY - base.y]
-    setStones((prev) => [...prev, [Math.round(x / C), Math.round(y / C)]])
+    const [rawX, rawY] = [e.clientX - base.x, e.clientY - base.y]
+    const p: Point = [adjustLine(rawX / C), adjustLine(rawY / C)]
+    setMoves((prev) => [...prev, p])
   }
   return (
     <div className="App">
       <svg width={16 * C} height={16 * C} onClick={onClick}>
-        { points.map(
+        { lines.map(
           (x, i) => <line key={i} x1={x * C} y1={1 * C} x2={x * C} y2={15 * C} stroke="black" />
         )
         }
-        { points.map(
+        { lines.map(
           (y, j) => <line key={j} x1={1 * C} y1={y * C} x2={15 * C} y2={y * C} stroke="black" />
         )
         }
-        { stones.map(
+        { moves.map(
           ([x, y], n) => {
             const fill = n % 2 === 0 ? 'black' : 'white'
             return <circle key={n} cx={x * C} cy={y * C} r={C / 2 - 1} fill={fill} stroke="black" />
@@ -39,6 +40,17 @@ const App: FC = () => {
       </svg>
     </div>
   )
+}
+
+const adjustLine = (n: number): Line => {
+  const r = Math.round(n)
+  if (r < 1) {
+    return 1
+  } else if (r > 15) {
+    return 15
+  } else {
+    return r as Line
+  }
 }
 
 export default App
