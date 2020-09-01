1e882dd35719b67537862e676e9445bac1598fae
yubessy <yubessy@gmail.com>
2020-09-01 22:29:36 +0900

    implement line

--- src/rule/line.ts
+++ src/rule/line.ts
@@ -2,9 +2,15 @@ import { N_INDICES } from './foundation'
 
 const N_DIAGONAL_LINES = N_INDICES * 2 - 1 // 29
 
-export type Line = boolean[]
+export type Line = {
+  size: number // length, between 1 and 15
+  bs: number // black stones as bit e.g. 0b00111010
+  ws: number // white stones as bit e.g. 0b01000100
+}
 
-export const newLine = (length: number): Line => new Array(length).fill(false)
+export const newLine = (size: number): Line => {
+  return { size: size, bs: 0b0, ws: 0b0 }
+}
 
 export type OrthogonalLines = Line[]
 
@@ -15,8 +21,8 @@ export const newOrthogonalLines = (): OrthogonalLines => new Array(N_INDICES).fi
 export const newDiagonalLines = (): DiagonalLines => new Array(N_DIAGONAL_LINES).fill(null).map(
   (_, i_) => {
     const i = i_ + 1
-    const length = i <= N_INDICES ? i : N_DIAGONAL_LINES - (i - 1)
-    return newLine(length)
+    const size = i <= N_INDICES ? i : N_DIAGONAL_LINES - (i - 1)
+    return newLine(size)
   }
 )
 
