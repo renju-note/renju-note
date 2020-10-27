3d93be5f4efc80bae861ec55f61ab33b9f027f81
Shotaro Tanaka <yubessy0@gmail.com>
2020-10-27 21:21:17 +0900

    add links and remarks (#2)

--- src/components/Main/Controller/AboutModal.tsx
+++ src/components/Main/Controller/AboutModal.tsx
@@ -1,4 +1,5 @@
 import {
+  Heading,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -6,10 +7,13 @@ import {
   ModalHeader,
   ModalFooter,
   ModalOverlay,
-  Text,
   Link,
+  List,
+  ListIcon,
+  ListItem,
 } from '@chakra-ui/core'
 import React, { FC } from 'react'
+import { FiGithub, FiTwitter } from 'react-icons/fi'
 
 type DefaultProps = {
   isOpen: boolean
@@ -24,13 +28,31 @@ const Default: FC<DefaultProps> = ({
     <Modal isOpen={isOpen} onClose={onClose} isCentered>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Renju Note</ModalHeader>
+        <ModalHeader as="h1">Renju Note</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Text>
-            If you find any bugs or problems,
-            tell <Link href="https://twitter.com/renjunotecom" color="teal.500" isExternal>@renjunotecom</Link>.
-          </Text>
+          <Heading as="h2" size="sm" mb="1rem">Links</Heading>
+          <List mb="1rem">
+            <ListItem>
+              <ListIcon icon={FiTwitter} />
+              <Link href="//twitter.com/renjunotecom" color="teal.500" isExternal>@renjunotecom</Link>
+              &nbsp;-&nbsp;Contact if any problems found.
+            </ListItem>
+            <ListItem>
+              <ListIcon icon={FiGithub} />
+              <Link href="//github.com/renju-note" color="teal.500" isExternal>@renju-note</Link>
+              &nbsp;-&nbsp;Source code is available here.
+            </ListItem>
+          </List>
+          <Heading as="h2" size="sm" mb="1rem">Remarks</Heading>
+          <List styleType="disc">
+            <ListItem>
+              Use this app on your own responsibility.
+            </ListItem>
+            <ListItem>
+              Some actions and events on this app are recorded by third-party analytics services in order to improve user experiences.
+            </ListItem>
+          </List>
         </ModalBody>
         <ModalFooter></ModalFooter>
       </ModalContent>
