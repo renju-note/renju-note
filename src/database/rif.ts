ec139f8d59819984a5709346525592afb77860cb
yubessy <yubessy0@gmail.com>
2020-11-04 01:37:53 +0900

    implement rif schema

--- src/database/rif.ts
+++ src/database/rif.ts
@@ -86,10 +86,10 @@ export type Game = Base & {
   btime: number
   wtime: number
   opening: Opening['id']
-  alt: string[]
+  alt: string
   swap: string
-  move: string[]
-  info?: string
+  move: string
+  info: string
 }
 tableFields[TableName.games] = '++id,publisher,tournament,round,rule,black,white,bresult,btime,wtime,opening,alt,swap,move,info'
 
@@ -123,11 +123,48 @@ export class RIFDatabase extends Dexie {
   async loadFromFile (file: File) {
     const text = await file.text()
     const dom = new DOMParser().parseFromString(text, 'application/xml')
+
     const countries = dom.querySelector(TableName.countries)
     if (countries !== null) {
       console.log(`Adding: ${TableName.countries}`)
       await this.addCountries(countries.children)
     }
+    const cities = dom.querySelector(TableName.cities)
+    if (cities !== null) {
+      console.log(`Adding: ${TableName.cities}`)
+      await this.addCities(cities.children)
+    }
+    const months = dom.querySelector(TableName.months)
+    if (months !== null) {
+      console.log(`Adding: ${TableName.months}`)
+      await this.addMonths(months.children)
+    }
+    const openings = dom.querySelector(TableName.openings)
+    if (openings !== null) {
+      console.log(`Adding: ${TableName.openings}`)
+      await this.addOpenings(openings.children)
+    }
+    const rules = dom.querySelector(TableName.rules)
+    if (rules !== null) {
+      console.log(`Adding: ${TableName.rules}`)
+      await this.addRules(rules.children)
+    }
+    const players = dom.querySelector(TableName.players)
+    if (players !== null) {
+      console.log(`Adding: ${TableName.players}`)
+      await this.addPlayers(players.children)
+    }
+    const tournaments = dom.querySelector(TableName.tournaments)
+    if (tournaments !== null) {
+      console.log(`Adding: ${TableName.tournaments}`)
+      await this.addTournaments(tournaments.children)
+    }
+    const games = dom.querySelector(TableName.games)
+    if (games !== null) {
+      console.log(`Adding: ${TableName.games}`)
+      await this.addGames(games.children)
+    }
+    console.log('Done')
   }
 
   private async addCountries (elems: HTMLCollection) {
@@ -159,7 +196,7 @@ export class RIFDatabase extends Dexie {
         country: parseInt(e.getAttribute('country') ?? ''),
       })
     }
-    await this.transaction('rw', this.countries, async () => {
+    await this.transaction('rw', this.cities, async () => {
       await this.cities.bulkAdd(items)
     })
   }
@@ -175,7 +212,7 @@ export class RIFDatabase extends Dexie {
         name: e.getAttribute('name') ?? '',
       })
     }
-    await this.transaction('rw', this.countries, async () => {
+    await this.transaction('rw', this.months, async () => {
       await this.months.bulkAdd(items)
     })
   }
@@ -192,8 +229,98 @@ export class RIFDatabase extends Dexie {
         info: e.getElementsByTagName('info').item(0)?.innerHTML ?? '',
       })
     }
-    await this.transaction('rw', this.countries, async () => {
+    await this.transaction('rw', this.rules, async () => {
       await this.rules.bulkAdd(items)
+    })
+  }
+
+  private async addOpenings (elems: HTMLCollection) {
+    const items: Opening[] = []
+    for (let i = 0; i < elems.length; i++) {
+      const e = elems[i]
+      const id = parseInt(e.id)
+      if (isNaN(id) || id < 0) continue
+      items.push({
+        id: id,
+        name: e.getAttribute('name') ?? '',
+        abbr: e.getAttribute('abbr') ?? '',
+
+      })
+    }
+    await this.transaction('rw', this.openings, async () => {
+      await this.openings.bulkAdd(items)
+    })
+  }
+
+  private async addPlayers (elems: HTMLCollection) {
+    const items: Player[] = []
+    for (let i = 0; i < elems.length; i++) {
+      const e = elems[i]
+      const id = parseInt(e.id)
+      if (isNaN(id) || id < 0) continue
+      items.push({
+        id: id,
+        name: e.getAttribute('name') ?? '',
+        surname: e.getAttribute('surname') ?? '',
+        country: parseInt(e.getAttribute('country') ?? ''),
+        city: parseInt(e.getAttribute('city') ?? ''),
+      })
+    }
+    await this.transaction('rw', this.players, async () => {
+      await this.players.bulkAdd(items)
+    })
+  }
+
+  private async addTournaments (elems: HTMLCollection) {
+    const items: Tournament[] = []
+    for (let i = 0; i < elems.length; i++) {
+      const e = elems[i]
+      const id = parseInt(e.id)
+      if (isNaN(id) || id < 0) continue
+      items.push({
+        id: id,
+        name: e.getAttribute('name') ?? '',
+        country: parseInt(e.getAttribute('country') ?? ''),
+        city: parseInt(e.getAttribute('city') ?? ''),
+        year: parseInt(e.getAttribute('year') ?? ''),
+        month: parseInt(e.getAttribute('month') ?? ''),
+        start: e.getAttribute('start') ?? '',
+        end: e.getAttribute('end') ?? '',
+        rule: parseInt(e.getAttribute('rule') ?? ''),
+        rated: (e.getAttribute('rated') ?? '') === '1',
+      })
+    }
+    await this.transaction('rw', this.tournaments, async () => {
+      await this.tournaments.bulkAdd(items)
+    })
+  }
+
+  private async addGames (elems: HTMLCollection) {
+    const items: Game[] = []
+    for (let i = 0; i < elems.length; i++) {
+      const e = elems[i]
+      const id = parseInt(e.id)
+      if (isNaN(id) || id < 0) continue
+      items.push({
+        id: id,
+        publisher: parseInt(e.getAttribute('publisher') ?? ''),
+        tournament: parseInt(e.getAttribute('tournament') ?? ''),
+        round: e.getAttribute('round') ?? '',
+        rule: parseInt(e.getAttribute('rule') ?? ''),
+        black: parseInt(e.getAttribute('black') ?? ''),
+        white: parseInt(e.getAttribute('white') ?? ''),
+        bresult: parseFloat(e.getAttribute('bresult') ?? ''),
+        btime: parseInt(e.getAttribute('btime') ?? ''),
+        wtime: parseInt(e.getAttribute('wtime') ?? ''),
+        opening: parseInt(e.getAttribute('opening') ?? ''),
+        alt: e.getAttribute('alt') ?? '',
+        swap: e.getAttribute('swap') ?? '',
+        move: e.getElementsByTagName('move').item(0)?.innerHTML ?? '',
+        info: e.getElementsByTagName('info').item(0)?.innerHTML ?? '',
+      })
+    }
+    await this.transaction('rw', this.games, async () => {
+      await this.games.bulkAdd(items)
     })
   }
 }
