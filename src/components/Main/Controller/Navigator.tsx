31e864c9d45da348488c379c06e5de77b2e507f2
yubessy <yubessy0@gmail.com>
2020-11-14 22:34:58 +0900

    refactor buttons

--- src/components/Main/Controller/Navigator.tsx
+++ src/components/Main/Controller/Navigator.tsx
@@ -1,6 +1,5 @@
 import {
-  Button,
-  Flex,
+  Button, ButtonGroup,
   IconButton
 } from '@chakra-ui/core'
 import React, { FC, useContext } from 'react'
@@ -10,25 +9,20 @@ import { BoardStateContext, SystemContext } from '../../contexts'
 const Default: FC = () => {
   const system = useContext(SystemContext)
   const { boardState, setBoardState } = useContext(BoardStateContext)
-  return <Flex justifyContent="center" alignItems="center">
+  return <ButtonGroup spacing={1} variant="ghost" size={system.buttonSize}>
     <IconButton
       onClick={() => setBoardState(boardState.toStart())}
       icon={FiChevronsLeft} aria-label="to start"
-      size={system.buttonSize}
-      variant="ghost"
       isDisabled={boardState.isStart}
     />
     <IconButton
       onClick={() => setBoardState(boardState.backward())}
       icon={FiChevronLeft} aria-label="backward"
-      size={system.buttonSize}
-      variant="ghost"
       isDisabled={boardState.isStart}
     />
     <Button
       width={6} // do not resize according to text
-      size={system.buttonSize}
-      variant="ghost" fontFamily="Noto Serif" fontWeight="normal"
+      fontFamily="Noto Serif" fontWeight="normal"
       isDisabled={true}
     >
       {boardState.cursor}
@@ -36,18 +30,14 @@ const Default: FC = () => {
     <IconButton
       onClick={() => setBoardState(boardState.forward())}
       icon={FiChevronRight} aria-label="forward"
-      size={system.buttonSize}
-      variant="ghost"
       isDisabled={boardState.isLast}
     />
     <IconButton
       onClick={() => setBoardState(boardState.toLast())}
       icon={FiChevronsRight} aria-label="to last"
-      size={system.buttonSize}
-      variant="ghost"
       isDisabled={boardState.isLast}
     />
-  </Flex>
+  </ButtonGroup>
 }
 
 export default Default
