1f025520e7d16abfe902333c739d0593f06ddbe3
yubessy <yubessy0@gmail.com>
2021-07-24 18:11:35 +0900

    move effects to root component

--- src/components/Advanced/SearchTab/index.tsx
+++ src/components/Advanced/SearchTab/index.tsx
@@ -1,21 +1,11 @@
 import { Box, Stack, Text } from '@chakra-ui/react'
-import { FC, useContext, useEffect, useMemo } from 'react'
-import { AnalyzedDatabase } from '../../../database'
-import { SearchResultState } from '../../../state'
+import { FC, useContext } from 'react'
 import { AdvancedContext } from '../../contexts'
 import SearchController from '../common/SearchController'
 import GamesSelector from './GamesSelector'
 
 const Default: FC = () => {
-  const db = useMemo(() => new AnalyzedDatabase(), [])
-  const { searchQueryState, searchResultState, setSearchResultState } = useContext(AdvancedContext)
-  const query = searchQueryState.query
-  useEffect(() => {
-    ;(async () => {
-      const { hit, ids, error } = await db.search(query)
-      setSearchResultState(new SearchResultState({ hit, gameIds: ids, error }))
-    })()
-  }, [query.moves?.toString(), query.playerId])
+  const { searchResultState } = useContext(AdvancedContext)
   return (
     <Stack justify="center" align="center">
       <Box width="100%">
--- src/components/Advanced/index.tsx
+++ src/components/Advanced/index.tsx
@@ -1,6 +1,7 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
-import { FC, useContext, useEffect } from 'react'
-import { TabName } from '../../state'
+import { FC, useContext, useEffect, useMemo } from 'react'
+import { AnalyzedDatabase } from '../../database'
+import { SearchResultState, TabName } from '../../state'
 import { AdvancedContext, BasicContext } from '../contexts'
 import DetailTab from './DetailTab'
 import MateTab from './MateTab'
@@ -8,14 +9,27 @@ import SearchTab from './SearchTab'
 import SetupTab from './SetupTab'
 
 const Default: FC = () => {
+  const db = useMemo(() => new AnalyzedDatabase(), [])
   const { boardState } = useContext(BasicContext)
-  const { tabsState, setTabsState, searchQueryState, setSearchQueryState } =
+  const { searchQueryState, setSearchQueryState, setSearchResultState } =
     useContext(AdvancedContext)
   const boardMoves = boardState.game.current.moves
+  const query = searchQueryState.query
   useEffect(() => {
     // TODO: error when game is inverted
     setSearchQueryState(searchQueryState.setMoves(boardMoves))
   }, [boardMoves.length])
+  useEffect(() => {
+    ;(async () => {
+      const { hit, ids, error } = await db.search(query)
+      setSearchResultState(new SearchResultState({ hit, gameIds: ids, error }))
+    })()
+  }, [query.moves?.toString(), query.playerId])
+  return <AdvancedTabs />
+}
+
+const AdvancedTabs: FC = () => {
+  const { tabsState, setTabsState } = useContext(AdvancedContext)
   return (
     <Tabs
       width="100%"
