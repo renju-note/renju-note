1e4bbd53c692bfbb202c8cc3f112a24eb552b0dc
yubessy <yubessy0@gmail.com>
2021-07-25 16:56:08 +0900

    refactor

--- src/analysis/encoding.ts
+++ src/analysis/encoding.ts
@@ -1,36 +1,17 @@
 import { Point } from '../rule/foundation'
-import { BitBoard, emptyBitBoard, encodeBitBoard } from './bitboard'
-import { variants } from './equivalence'
+import { canonicalBitboardString, emptyBitboardVariants, pointsVariants } from './variation'
 
 export const encodeAccumulatedMoves = (moves: Point[]): string[] => {
-  const movesVariants = variants(moves)
-  const boardVariants: BitBoard[] = [
-    emptyBitBoard(),
-    emptyBitBoard(),
-    emptyBitBoard(),
-    emptyBitBoard(),
-    emptyBitBoard(),
-    emptyBitBoard(),
-    emptyBitBoard(),
-    emptyBitBoard(),
-  ]
+  const boardVariants = emptyBitboardVariants()
+  const movesVariants = pointsVariants(moves)
   const result: string[] = []
   for (let i = 0; i < moves.length; i++) {
-    const candidates: string[] = []
+    const black = i % 2 === 0
     for (let v = 0; v < movesVariants.length; v++) {
-      const [x, y] = movesVariants[v][0][i]
-      boardVariants[v][x - 1] += ((i % 2) + 1) << (2 * (y - 1))
-      candidates[v] = encodeBitBoard(boardVariants[v])
+      const point = movesVariants[v][i]
+      boardVariants[v].putMut(black, point)
     }
-    result[i] = stringMin(candidates)
+    result[i] = canonicalBitboardString(boardVariants)
   }
   return result
-}
-
-const stringMin = (ss: string[]): string => {
-  let min = ss[0]
-  for (let i = 1; i < ss.length; i++) {
-    if (ss[i] < min) min = ss[i]
-  }
-  return min
 }
--- src/analysis/index.ts
+++ src/analysis/index.ts
@@ -1,3 +1,3 @@
 export * from './bitboard'
 export * from './encoding'
-export * from './equivalence'
+export * from './variation'
